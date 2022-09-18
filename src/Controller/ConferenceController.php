13f242ee9585ccf35dde749c8de42325723a1f3d
Christophe <christophe@clopez.fr>
2022-09-18 12:37:14 +0200

    dump the index controller

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -3,22 +3,25 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ConferenceController extends AbstractController
 {
     #[Route('/', name: 'homepage')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
 //        return $this->render('conference/index.html.twig', [
 //            'controller_name' => 'ConferenceController',
 //        ]);
 
+        dump($request);
+
         return new Response(<<<EOF
-         <html>
+         <html lang="fr">
          <body>
-         <img src="/images/under_construction.png" />
+         <img src="/images/under_construction.png"  alt="under construction"/>
          </body>
          </html>
          EOF
